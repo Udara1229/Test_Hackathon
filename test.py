72033cb13488d900c6368d93089796f4c7375281
uviniB98 <uvinibalasuriya98@gmail.com>
2021-06-25 21:33:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,26 @@
 
 #test file
 
+import cv2
+import pytesseract
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+img = cv2.imread("C:/Users/UVINI BALASURIYA/Downloads/stat-circle-workshop-test-main/stat-circle-workshop-test-main/pytesseact_demo/demo_code/images/002.png") # read an image
+
+
+img
+
+
+
+
+
+
